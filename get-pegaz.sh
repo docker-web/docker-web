bf3371b84d1df9cdd95ce3c817efa55441faee65
valere <dev@valerebron.com>
2022-04-25 23:43:31 +0200

    FIX: try new synthax

--- get-pegaz.sh
+++ get-pegaz.sh
@@ -6,11 +6,11 @@ PEGAZ_GITHUB=https://github.com/valerebron/pegaz
 PEGAZ_PATH=/etc/pegaz
 
 INSTALL_GIT() {
-  if ![type git > /dev/null]; then
-    if ![type apt > /dev/null]; then
+  if ! [command -v git &> /dev/null]; then
+    if ! [command -v apt &> /dev/null]; then
       echo "pegaz :: install GIT"
       apt update -y && apt upgrade -y && apt install -y git
-    elif [type apk > /dev/null]; then
+    elif ! [command -v apk &> /dev/null]; then
       echo "pegaz :: install GIT"
       apk update && apk add git
     else
@@ -23,7 +23,7 @@ INSTALL_GIT() {
   fi
 }
 INSTALL_DOCKER() {
-  if ![type docker > /dev/null]; then
+  if ! [command -v docker &> /dev/null]; then
     echo "pegaz :: install DOCKER"
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh get-docker.sh
@@ -37,16 +37,16 @@ INSTALL_DOCKER() {
 
 INSTALL_GIT
 INSTALL_DOCKER
-if ![ docker network ls | grep pegaz ]; then
+if ! [docker network ls | grep pegaz]; then
   echo "pegaz :: create NETWORK"
   docker network create pegaz
 fi
-if ![ EXIST $PEGAZ_PATH/pegaz.sh ]; then
+if ! [EXIST $PEGAZ_PATH/pegaz.sh]; then
   echo "pegaz :: clone PROJECT"
   git clone $PEGAZ_GITHUB $PEGAZ_PATH
   chmod +x $PEGAZ_PATH/pegaz.sh
 fi
-if ![ grep -q pegaz /etc/bash.bashrc &> /dev/null ]; then
+if ! [grep -q pegaz /etc/bash.bashrc &> /dev/null]; then
   echo "pegaz :: create ALIAS"
   echo "alias pegaz='sh $PEGAZ_PATH/pegaz.sh $1 $2'" >> /etc/bash.bashrc
 fi
