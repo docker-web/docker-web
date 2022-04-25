c6aefbdc731d5ff5707c5872638a5cb3e61e2429
valere <dev@valerebron.com>
2022-04-25 09:38:48 +0200

    FIX: add apt update before

--- get-pegaz.sh
+++ get-pegaz.sh
@@ -6,6 +6,7 @@ PEGAZ_PATH=/etc/pegaz
 INSTALL_GIT() {
   if ! [command -v git &> /dev/null]; then
     if [command -v apt &> /dev/null]; then
+      apt update
       apt install -y git
     elif [command -v apk &> /dev/null]; then
       apk add git
