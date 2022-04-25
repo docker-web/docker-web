46232307f8c6584da25c820f03bcd94330ea7ecb
valere <dev@valerebron.com>
2022-04-26 00:03:43 +0200

    FIX: try new synthax

--- get-pegaz.sh
+++ get-pegaz.sh
@@ -6,11 +6,11 @@ PEGAZ_GITHUB=https://github.com/valerebron/pegaz
 PEGAZ_PATH=/etc/pegaz
 
 INSTALL_GIT() {
-  if [command -v git &> /dev/null]; then
-    if ! [command -v apt &> /dev/null]; then
+  if [$(wich git) &> /dev/null]; then
+    if ! [$(wich apt) &> /dev/null]; then
       echo "pegaz :: install GIT"
       apt update -y && apt upgrade -y && apt install -y git
-    elif ! [command -v apk &> /dev/null]; then
+    elif ! [$(wich apk) &> /dev/null]; then
       echo "pegaz :: install GIT"
       apk update && apk add git
     else
@@ -23,7 +23,7 @@ INSTALL_GIT() {
   fi
 }
 INSTALL_DOCKER() {
-  if [command -v docker &> /dev/null]; then
+  if [$(wich docker) &> /dev/null]; then
     echo "pegaz :: install DOCKER"
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh get-docker.sh
