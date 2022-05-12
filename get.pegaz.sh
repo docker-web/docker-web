5ee31ed3d6bd0abf3f97f08d291c62fe3ad9f213
valere <dev@valerebron.com>
2022-05-12 12:28:07 +0200

    FEAT: improve pegazdev & FIX: docker sock for nginx

--- get.pegaz.sh
+++ get.pegaz.sh
@@ -61,7 +61,7 @@ CREATE_ALIAS() {
   then
     echo "create ALIAS"
     echo "alias pegaz='bash $PATH_PEGAZ/pegaz-cli.sh \$1 \$2'" >> /etc/bash.bashrc
-    echo "alias pegazdev='sudo cp -ra ./* $PATH_PEGAZ && bash pegaz-cli.sh \$1 \$2'" >> /etc/bash.bashrc
+    echo "alias pegazdev='pwd | grep -q pegaz && sudo cp -ra ./* $PATH_PEGAZ && bash pegaz-cli.sh \$1 \$2'" >> /etc/bash.bashrc
     source /etc/bash.bashrc
   else
     echo "skip ALIAS"
