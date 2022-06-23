236ad3171c7864935052a8a049c3788a166e5286
valere <dev@valerebron.com>
2022-06-23 15:52:32 +0200

    FEAT: add storj to nextcloud

--- services/nextcloud/config.sh
+++ services/nextcloud/config.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export SUBDOMAIN="cloud"
-export PORT="7710"
-export PORT_DB="7711"
-export APPS_PRE_INSTALLED="calendar contacts mail notes music spreed extract metadata richdocuments richdocumentscode camerarawpreviews"
+export PORT="7704"
+export PORT_EXPOSED="8080"
+export PORT_DB="7705"
+export APPS_PRE_INSTALLED="calendar contacts mail notes music spreed extract metadata richdocuments richdocumentscode camerarawpreviews sharerenamer storj"
 export APPS_ENABLED="files_external"
 export APPS_DISABLED="activity photos recommendations dashboard"
 export PHP_MEMORY_LIMIT="4G"
