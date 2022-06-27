3d285f7fe4023159c0630474aad7a282b5aa3fa7
valere <dev@valerebron.com>
2022-06-28 00:59:52 +0200

    FEAT: create completion v1 !

--- completion.sh
+++ completion.sh
@@ -4,28 +4,26 @@ _pegaz() {
   source "/opt/pegaz/env.sh"
   SERVICES=$(find $PATH_PEGAZ_SERVICES -mindepth 1 -maxdepth 1 -not -name '.*' -type d -printf '  %f\n' | sort | sed '/^$/d')
 
-  local cur prev prev2
-  COMPREPLY=()
-  FIRST="${COMP_WORDS[COMP_CWORD]}"
-  SECOND="${COMP_WORDS[COMP_CWORD+1]}"
-  THIRD="${COMP_WORDS[COMP_CWORD+2]}"
+  local cur prev
+  CUR="${COMP_WORDS[COMP_CWORD]}"
+  PREV="${COMP_WORDS[COMP_CWORD-1]}"
+  PREV_PREV="${COMP_WORDS[COMP_CWORD-2]}"
 
   if test $COMP_CWORD -eq 1
   then
-    COMPREPLY=( $(compgen -W "${COMMANDS[*]}" -- ${FIRST}) )
+    COMPREPLY=( $(compgen -W "${COMMANDS[*]}" -- ${CUR}) )
   elif test $COMP_CWORD -eq 2
   then
-    case "$SECOND" in
-      ${COMMANDS_CORE// /|}) return 0;;
-      *) COMPREPLY=($(compgen -W "$SERVICES" -- ${SECOND}))
-    esac
+    if [[ $COMMANDS_CORE =~ $PREV ]]
+    then
+      return 0
+    else
+      COMPREPLY=($(compgen -W "$SERVICES" -- ${CUR}))
+    fi
   elif test $COMP_CWORD -eq 3
   then
-    case "$THIRD" in
-      create)
-        RESULTS=$(docker search $THIRD --limit 1 --format "{{.Name}}")
-        echo $RESULTS
-        COMPREPLY=($(compgen -W "$RESULTS" -- ${THIRD}))
+    case "$PREV_PREV" in
+      create) IMAGES=$(docker search $PREV --limit 7 --format "{{.Name}}") && COMPREPLY=($(compgen -W "$IMAGES" -- ${CUR}))
     esac
   fi
 
