790bf9a651fb2eb1d8be79338eecdf09ab9d7310
valere <dev@valerebron.com>
2022-06-27 12:39:59 +0200

    FIX:order get.pegaz

--- get.pegaz.sh
+++ get.pegaz.sh
@@ -3,48 +3,6 @@
 
 source <(curl -s https://raw.githubusercontent.com/valerebron/pegaz/master/env.sh)
 
-INSTALL_DEPS() {
-  # get distrib
-  # completion ?
-  # install sudo
-  # install bash
-  apk update
-  apk upgrade
-  apk add bash
-}
-
-CLONE_PROJECT() {
-  if ! test -d $PATH_PEGAZ
-  then
-    sudo mkdir -p $PATH_PEGAZ $DATA_DIR
-    sudo git clone $GITHUB_PEGAZ $PATH_PEGAZ
-    sudo chown -R $USER:$USER $PATH_PEGAZ
-    sudo chmod -R 750 $PATH_PEGAZ
-  fi
-}
-
-INSTALL_DOCKER() {
-  if ! type docker 1>/dev/null
-  then
-    echo "install docker"
-    curl -fsSL https://get.docker.com | bash
-    usermod -aG docker $USER
-    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  fi
-}
-
-INSTALL_CLI() {
-  if ! echo $(cat $PATH_BASHRC) | grep -q cli.pegaz.sh
-  then
-    echo "install cli"
-    echo "alias pegaz='bash $PATH_PEGAZ/cli.pegaz.sh \$1 \$2'" | sudo tee -a $PATH_BASHRC  >/dev/null
-    echo "alias pegazdev='pwd | grep -q pegaz && rm -rf $PATH_PEGAZ/* && cp -ra ./* $PATH_PEGAZ && bash cli.pegaz.sh \$1 \$2'" | sudo tee -a $PATH_BASHRC  >/dev/null
-    echo ". $PATH_PEGAZ/completion.sh" | sudo tee -a $PATH_BASHRC  >/dev/null
-    complete -F _pegaz pegaz pegazdev
-    exec bash
-  fi
-}
-
 INSTALL_GIT() {
   if ! type git 1>/dev/null
   then
@@ -62,6 +20,38 @@ INSTALL_GIT() {
     fi
   else
     return 0
+  fi
+}
+
+INSTALL_DOCKER() {
+  if ! type docker 1>/dev/null
+  then
+    echo "install docker"
+    curl -fsSL https://get.docker.com | bash
+    usermod -aG docker $USER
+    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  fi
+}
+
+CLONE_PROJECT() {
+  if ! test -d $PATH_PEGAZ
+  then
+    sudo mkdir -p $PATH_PEGAZ $DATA_DIR
+    sudo git clone $GITHUB_PEGAZ $PATH_PEGAZ
+    sudo chown -R $USER:$USER $PATH_PEGAZ
+    sudo chmod -R 750 $PATH_PEGAZ
+  fi
+}
+
+INSTALL_CLI() {
+  if ! echo $(cat $PATH_BASHRC) | grep -q cli.pegaz.sh
+  then
+    echo "install cli"
+    echo "alias pegaz='bash $PATH_PEGAZ/cli.pegaz.sh \$1 \$2'" | sudo tee -a $PATH_BASHRC  >/dev/null
+    echo "alias pegazdev='pwd | grep -q pegaz && rm -rf $PATH_PEGAZ/* && cp -ra ./* $PATH_PEGAZ && bash cli.pegaz.sh \$1 \$2'" | sudo tee -a $PATH_BASHRC  >/dev/null
+    echo ". $PATH_PEGAZ/completion.sh" | sudo tee -a $PATH_BASHRC  >/dev/null
+    complete -F _pegaz pegaz pegazdev
+    exec bash
   fi
 }
 
