64b22662a21851f8fa9e1a71dff4bb42b856adaf
valere <dev@valerebron.com>
2022-06-30 14:53:29 +0200

    FEAT: gitlab v0.1

---
+++ services/gitlab/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export IMAGE="gitlab/gitlab-ce:latest"
+export SUBDOMAIN="git"
+export PORT="7722"
+export PORT_HTTPS="7723"
+export PORT_SSH="7724"
+export PORT_EXPOSED="80"
+export REDIRECTIONS=""
