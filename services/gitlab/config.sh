e8902f7a26f0c71724df4a7f5eaa54ba714d4693
valere <dev@valerebron.com>
2022-07-02 12:09:54 +0200

    FIX: drop gitlab

--- services/gitlab/config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export IMAGE="gitlab/gitlab-ce:latest"
-export SUBDOMAIN="git"
-export PORT="7722"
-export PORT_HTTPS="7723"
-export PORT_SSH="7724"
-export PORT_EXPOSED="80"
-export REDIRECTIONS=""
