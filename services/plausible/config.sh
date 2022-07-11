77d2c737d9d1b2e7cfec4e6f3d5cf7625b5e679b
valere <dev@valerebron.com>
2022-07-11 12:47:28 +0200

    FIX: plausible secret key auto gen for pagazdev

--- services/plausible/config.sh
+++ services/plausible/config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export IMAGE="plausible/analytics"
+export IMAGE="plausible/analytics:v1.4.4"
 export SUBDOMAIN="plausible"
 export PORT="7717"
 export PORT_DB="7718"
 export PORT_EXPOSED="8000"
+export SECRET_KEY_BASE="owVACy9fWJK1eonJaXmIFclXSWygNdLQI9kXBlnSMrRNFetOMpQeSlu5R4rrbfye M873ZBixhrAU31ERqi+m8Q==" # replaced by pre-install.sh"
 export REDIRECTIONS=""
--- services/plausible/pre-install.sh
+++ services/plausible/pre-install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "pre install here"
+echo "path: $PATH_PEGAZ_SERVICES"
+echo $(pwd)
 SSL=$(echo $(printf '%s\n' $(openssl rand -base64 64)) | sed 's/ /\\ /g')
-sed -i -e "s|replaced-by-pre-install|$SSL|g" "$PATH_PEGAZ_SERVICES/$1/docker-compose.yml"
+sed -i "s|SECRET_KEY_BASE=.*|SECRET_KEY_BASE=\"$SSL\"|g" "$PATH_PEGAZ_SERVICES/$1/config.sh"
