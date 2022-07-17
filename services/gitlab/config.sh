b96235889756ee348d5df2c5105c007039a03132
valere <dev@valerebron.com>
2022-07-17 14:21:56 +0200

    FEAT: gitlab add runner config & try port 80

--- services/gitlab/config.sh
+++ services/gitlab/config.sh
@@ -2,6 +2,6 @@
 export IMAGE="gitlab/gitlab-ce"
 export SUBDOMAIN="git"
 export PORT="7737"
-export PORT_EXPOSED="22"
+export PORT_EXPOSED="80"
 export REDIRECTIONS=""
 export POST_INSTALL_TEST_CMD=""
