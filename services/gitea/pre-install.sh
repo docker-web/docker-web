79e2ee20746d36f8207b5d4793c55f848a454ff8
valere <dev@valerebron.com>
2022-07-18 14:53:16 +0200

    FIX: gitea dyn proto for drone & delete pre cofig ssh keygen unused code

--- services/gitea/pre-install.sh
+++ services/gitea/pre-install.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-cat <<"EOF" | sudo tee /usr/local/bin/gitea
-#!/bin/sh
-ssh -p $GIT_SSH_PORT -o StrictHostKeyChecking=no git@127.0.0.1 \
-"SSH_ORIGINAL_COMMAND=\"$SSH_ORIGINAL_COMMAND\" $0 $@"
-EOF
-sudo chmod +x /usr/local/bin/gitea
-
 sudo useradd git
 GIT_UID=$(id -u git)
 GIT_GID=$(id -g git)
 
-sudo su git -c ssh-add-key git@localhost -p $PORT_SSH
-sed -i "s|PROTO=.*|PROTO=\"https\"|g" config.sh
+[[ $IS_PEGAZDEV == "false" ]] && sed -i "s|PROTO=.*|PROTO=\"https\"|g" "$PATH_PEGAZ_SERVICES/$1/config.sh"
