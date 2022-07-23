27864a2c8bb7486b7ab1cf7a3c084d1d1d8b8e98
prosety <dev@prosety.com>
2022-07-23 21:50:15 +0200

    GITEA: v2 user creation works

--- services/gitea/pre-install.sh
+++ services/gitea/pre-install.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-# SSH Container Passthrough 
 
 sudo useradd git
 sudo chown -R git:git /home/git/
 
-GIT_UID=$(id -u git)
-GIT_GID=$(id -g git)
-
-sudo -u git ssh-keygen -t rsa -b 4096 -C "Gitea Host Key" -N "" -f "/home/git/.ssh/id_rsa" -y
-sudo -u git cat /home/git/.ssh/id_rsa.pub | sudo -u git tee -a /home/git/.ssh/authorized_keys
+# SSH Container Passthrough
+sudo -u git ssh-keygen -t rsa -b 4096 -C "Gitea Host Key" -N "" -f "/home/git/.ssh/id_rsa" -y > /dev/null
+sudo -u git cat /home/git/.ssh/id_rsa.pub | sudo -u git tee -a /home/git/.ssh/authorized_keys > /dev/null
 sudo -u git chmod 750 /home/git/.ssh/authorized_keys
-
 sudo echo -e "#!/bin/bash
-ssh -p ${PORT_SSH} -o StrictHostKeyChecking=no git@127.0.0.1 \"SSH_ORIGINAL_COMMAND=\\\"\$SSH_ORIGINAL_COMMAND\\\" \$0 \$@\"" | sudo tee /usr/local/bin/gitea
-
+ssh -p ${PORT_SSH} -o StrictHostKeyChecking=no git@127.0.0.1 \"SSH_ORIGINAL_COMMAND=\\\"\$SSH_ORIGINAL_COMMAND\\\" \$0 \$@\"" | sudo tee /usr/local/bin/gitea > /dev/null
 sudo chmod +x /usr/local/bin/gitea
 
+# switch between http / https dev / prod
 [[ $IS_PEGAZDEV == "false" ]] && sed -i "s|PROTO=.*|PROTO=\"https\"|g" "$PATH_PEGAZ_SERVICES/$1/config.sh"
