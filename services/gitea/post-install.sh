e8a5042665feca701e3818d4e0e979670788f3f2
prosety <dev@prosety.com>
2022-07-24 11:57:52 +0200

    GITEA: drone will be setup manually & need to restart

--- services/gitea/post-install.sh
+++ services/gitea/post-install.sh
@@ -8,7 +8,8 @@ sleep 5
 GITEA "admin create-user --admin --username $USERNAME --password $PASSWORD --email $EMAIL --must-change-password=false"
 
 # Manuel Drone configuration :
-# Create OAuth2 Applications via web ui
+# Create OAuth2 Applications via web ui:
+# name: drone, redirect uri: http://drone.domain.com/login
 # Copy ID & SECRET to config.sh
-# restart drone
-# source config.sh && source services/gitea/config.sh && docker-compose -f services/gitea/docker-compose.yml up drone
+# restart drone:
+# source config.sh && source services/gitea/config.sh && docker-compose -f services/gitea/docker-compose.yml up -d drone
