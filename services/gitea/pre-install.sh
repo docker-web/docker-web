5cd54599e38088df952f5aac07d033a8cdd2622a
prosety <dev@prosety.com>
2022-07-24 12:56:27 +0200

    GITEA: auto install sudo

--- services/gitea/pre-install.sh
+++ services/gitea/pre-install.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# gitea pre install only on root !
-[[ ${EUID} -ne 0 ]] && printf "[x] up gitea must be run as root.'\n" && exit
+if ! command -v sudo 1>/dev/null
+then
+echo "[*] install sudo"
+command -v apt 1>/dev/null && apt update --allow-releaseinfo-change -y && apt -y install sudo
+command -v apk 1>/dev/null && apk update && apk add sudo
+command -v pacman 1>/dev/null && pacman -Sy --noconfirm sudo
+command -v yum 1>/dev/null && yum -y update && yum -y install sudo
+fi
 
-useradd git
-chown -R git:git /home/git/
+sudo useradd git
+sudo chown -R git:git /home/git/
 
 # SSH Container Passthrough
--u git ssh-keygen -t rsa -b 4096 -C "Gitea Host Key" -N "" -f "/home/git/.ssh/id_rsa" -y > /dev/null
--u git cat /home/git/.ssh/id_rsa.pub | -u git tee -a /home/git/.ssh/authorized_keys > /dev/null
--u git chmod 750 /home/git/.ssh/authorized_keys
-echo -e "#!/bin/bash
-ssh -p ${PORT_SSH} -o StrictHostKeyChecking=no git@127.0.0.1 \"SSH_ORIGINAL_COMMAND=\\\"\$SSH_ORIGINAL_COMMAND\\\" \$0 \$@\"" | tee /usr/local/bin/gitea > /dev/null
-chmod +x /usr/local/bin/gitea
+sudo -u git ssh-keygen -t rsa -b 4096 -C "Gitea Host Key" -N "" -f "/home/git/.ssh/id_rsa" -y > /dev/null
+sudo -u git cat /home/git/.ssh/id_rsa.pub | sudo -u git tee -a /home/git/.ssh/authorized_keys > /dev/null
+sudo -u git chmod 750 /home/git/.ssh/authorized_keys
+sudo echo -e "#!/bin/bash
+ssh -p ${PORT_SSH} -o StrictHostKeyChecking=no git@127.0.0.1 \"SSH_ORIGINAL_COMMAND=\\\"\$SSH_ORIGINAL_COMMAND\\\" \$0 \$@\"" | sudo tee /usr/local/bin/gitea > /dev/null
+sudo chmod +x /usr/local/bin/gitea
 
 # switch between http / https dev / prod
 [[ $IS_PEGAZDEV == "false" ]] && sed -i "s|PROTO=.*|PROTO=\"https\"|g" "$PATH_PEGAZ_SERVICES/$1/config.sh"
