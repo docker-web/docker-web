e926c324991c37d8afda56c3daea86d993b12f60
prosety <dev@prosety.com>
2022-07-24 12:48:46 +0200

    GITEA: pre install only on root

--- services/gitea/pre-install.sh
+++ services/gitea/pre-install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-sudo useradd git
-sudo chown -R git:git /home/git/
+# gitea pre install only on root !
+[[ ${EUID} -ne 0 ]] && printf "[x] up gitea must be run as root.'\n" && exit
+
+useradd git
+chown -R git:git /home/git/
 
 # SSH Container Passthrough
-sudo -u git ssh-keygen -t rsa -b 4096 -C "Gitea Host Key" -N "" -f "/home/git/.ssh/id_rsa" -y > /dev/null
-sudo -u git cat /home/git/.ssh/id_rsa.pub | sudo -u git tee -a /home/git/.ssh/authorized_keys > /dev/null
-sudo -u git chmod 750 /home/git/.ssh/authorized_keys
-sudo echo -e "#!/bin/bash
-ssh -p ${PORT_SSH} -o StrictHostKeyChecking=no git@127.0.0.1 \"SSH_ORIGINAL_COMMAND=\\\"\$SSH_ORIGINAL_COMMAND\\\" \$0 \$@\"" | sudo tee /usr/local/bin/gitea > /dev/null
-sudo chmod +x /usr/local/bin/gitea
+-u git ssh-keygen -t rsa -b 4096 -C "Gitea Host Key" -N "" -f "/home/git/.ssh/id_rsa" -y > /dev/null
+-u git cat /home/git/.ssh/id_rsa.pub | -u git tee -a /home/git/.ssh/authorized_keys > /dev/null
+-u git chmod 750 /home/git/.ssh/authorized_keys
+echo -e "#!/bin/bash
+ssh -p ${PORT_SSH} -o StrictHostKeyChecking=no git@127.0.0.1 \"SSH_ORIGINAL_COMMAND=\\\"\$SSH_ORIGINAL_COMMAND\\\" \$0 \$@\"" | tee /usr/local/bin/gitea > /dev/null
+chmod +x /usr/local/bin/gitea
 
 # switch between http / https dev / prod
 [[ $IS_PEGAZDEV == "false" ]] && sed -i "s|PROTO=.*|PROTO=\"https\"|g" "$PATH_PEGAZ_SERVICES/$1/config.sh"
