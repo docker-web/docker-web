d1077cdc1e38eed475a1481355d6a831457e2fec
prosety <dev@prosety.com>
2022-07-24 17:25:42 +0200

    GITEA: reset sudo install on preinstall

--- services/gitea/pre-install.sh
+++ services/gitea/pre-install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if ! command -v sudo 1>/dev/null
+then
+    echo "[*] install sudo"
+    command -v apt 1>/dev/null && apt update --allow-releaseinfo-change -y && apt -y install sudo
+    command -v apk 1>/dev/null && apk update && apk add sudo
+    command -v pacman 1>/dev/null && pacman -Sy --noconfirm sudo
+    command -v yum 1>/dev/null && yum -y update && yum -y install sudo
+fi
+
 sudo useradd git >/dev/null 2>&1
 sudo chown -R git:git /home/git/
 
