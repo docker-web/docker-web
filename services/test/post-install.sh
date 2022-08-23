833ab00349505ce2f60cbf367dd8241ec8654f68
prosety <dev@prosety.com>
2022-08-23 12:30:14 +0200

    TEST: improve test page

--- services/test/post-install.sh
+++ services/test/post-install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker exec $1 sed -i 's/Welcome\ to\ nginx!/Pegaz test page/' /usr/share/nginx/html/index.html
+docker cp docs/pegaz.svg $1:/usr/share/nginx/html/
+docker exec $1 sed -i "0,/<body>/s//<body>\n<img style=\"width:100%\" src=\"pegaz.svg\"\/>/" "/usr/share/nginx/html/index.html"
+docker exec $1 sed -i 's/Welcome\ to\ nginx!/pegaz test page/' /usr/share/nginx/html/index.html
 docker exec $1 sed -i 's/If you see this page/this file was edited with post-install.sh/' /usr/share/nginx/html/index.html
