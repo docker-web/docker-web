af16a528a34a4a6fae43e58ddb298a3aa42e8bd4
prosety <dev@prosety.com>
2022-08-26 11:22:04 +0200

    FIX: docker should install when sudo bash get.pegaz (so with another user as root), but docker installtion failed !

--- get.pegaz.sh
+++ get.pegaz.sh
@@ -23,12 +23,12 @@ INSTALL_DOCKER() {
   then
     echo "[*] install docker :"
     apt update -y
-    curl -fsSL https://get.docker.com | bash
-    groupadd docker
-    usermod -aG docker $USER
+    sudo curl -fsSL https://get.docker.com | sudo bash
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
     if [[ -n $SUDO_USER ]]
     then
-      usermod -aG docker $SUDO_USER
+      sudo usermod -aG docker $SUDO_USER
     fi
     echo "[*] install docker-compose :"
     curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
