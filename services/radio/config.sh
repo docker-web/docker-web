e4bb35adae53e2a03d491a0d0e27b1a398ce8a61
prosety <dev@prosety.com>
2022-09-04 15:21:31 +0200

    FIX: rename & reorder radio

--- services/radio/config.sh
+++ services/radio/config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-export IMAGE="phasecorex/liquidsoap"
+export IMAGE="infiniteproject/icecast"
 export DOMAIN="radio.$MAIN_DOMAIN"
+export DOMAIN_LIQ="liq.$MAIN_DOMAIN"
 export PORT="7727"
 export PORT_EXPOSED="8000"
 export PORT_LIQUIDSOAP="7728"
