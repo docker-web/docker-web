311dde668be363d7690ec33516790b8bba7cbb4d
prosety <dev@prosety.com>
2022-09-21 10:53:10 +0200

    POST-INSTALL: wait 3seconds before launch

--- cli.pegaz.sh
+++ cli.pegaz.sh
@@ -123,9 +123,9 @@ POST_INSTALL() {
   if [[ $? -eq 0 ]]
   then
     local POST_INSTALL_TEST_CMD=""
-    source $PATH_PEGAZ_SERVICES/$1/config.sh
+    source "$PATH_PEGAZ_SERVICES/$1/config.sh"
     local PATH_SCRIPT="$PATH_PEGAZ_SERVICES/$1/$FILENAME_POSTINSTALL"
-    if test -f $PATH_SCRIPT
+    if [[ -f $PATH_SCRIPT ]]
     then
       echo "[*] post-install: wait for $1 up"
       if [[ -n $POST_INSTALL_TEST_CMD ]]
@@ -135,6 +135,7 @@ POST_INSTALL() {
           $POST_INSTALL_TEST_CMD >> /dev/null
           if [[ $? -eq 0 ]]
           then
+            wait 3
             echo "[*] $POST_INSTALL_TEST_CMD is enable, launch post-install.sh"
             bash $PATH_SCRIPT $1 &&\
             SERVICE_INFOS $1
@@ -150,6 +151,7 @@ POST_INSTALL() {
           HTTP_CODE=$(curl -ILs $DOMAIN | head -n 1 | cut -d$' ' -f2)
           if [[ $HTTP_CODE < "400" ]]
           then
+            wait 3
             echo "[*] $DOMAIN http status code is $HTTP_CODE, launch post-install.sh"
             bash $PATH_SCRIPT $1 &&\
             SERVICE_INFOS $1
