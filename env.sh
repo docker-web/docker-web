6332ea2d7aff48f7d14b1b2a8a76e5e064192c1c
prosety <dev@prosety.com>
2022-09-26 09:05:51 +0200

    v1.1.3

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PEGAZ_VERSION="1.1.2"
+export PEGAZ_VERSION="1.1.3"
 export GITHUB_PEGAZ="https://github.com/valerebron/pegaz"
 export PATH_PEGAZ="/opt/pegaz"
 export PATH_PEGAZ_SERVICES="$PATH_PEGAZ/services"
