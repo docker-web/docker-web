a397e93e7ac4a89f939a0a990771f739d0133c2f
prosety <dev@prosety.com>
2022-09-30 12:07:03 +0200

    FIX: small mistake 2

--- services/proxy/config.sh
+++ services/proxy/config.sh
@@ -2,4 +2,4 @@
 export IMAGE="nginxproxy/nginx-proxy"
 export PORT_HTTP="80"
 export PORT_HTTPS="443"
-export ACME_CA_URI: "https://acme.zerossl.com/v2/DV90"
+export ACME_CA_URI="https://acme.zerossl.com/v2/DV90"
