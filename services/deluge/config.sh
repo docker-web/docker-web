4353b736f88fa95b2f51f80257a1bd62c96aa49b
prosety <dev@prosety.com>
2022-10-05 09:51:50 +0200

    FEAT: first debug for http in prod

--- services/deluge/config.sh
+++ services/deluge/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export IMAGE="linuxserver/deluge"
 export DOMAIN="deluge.$MAIN_DOMAIN"
-export PORT="7732"
+export PORT="8112"
 export PORT_EXPOSED="8112"
 export REDIRECTIONS=""
 export POST_INSTALL_TEST_CMD="docker exec deluge ls"
