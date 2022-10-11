f44ffacc56bc594e098206acde0ed94dd11ccff7
prosety <dev@prosety.com>
2022-10-11 16:23:43 +0200

    FIX: dashboard if no logo

--- services/drone/post-install.sh
+++ services/drone/post-install.sh
@@ -6,3 +6,17 @@
 # Copy ID & SECRET to config.sh
 # re-launch gitea stack:
 # pegaz up drone
+# Install host exec runner
+# curl -L https://github.com/drone-runners/drone-runner-exec/releases/latest/download/drone_runner_exec_linux_amd64.tar.gz | tar zx
+# sudo install -t /usr/local/bin drone-runner-exec
+# config
+# /etc/drone-runner-exec/config
+# DRONE_RPC_PROTO=https
+# DRONE_RPC_HOST=drone.company.com
+# DRONE_RPC_SECRET=super-duper-secret
+# DRONE_LOG_FILE=drone-runner-exec/log.txt
+# start
+# sudo drone-runner-exec service install
+# sudo drone-runner-exec service start
+# test
+# cat drone-runner-exec/log.txt
--- services/drone/pre-install.sh
+++ services/drone/pre-install.sh
@@ -3,7 +3,9 @@
 # switch between http / https dev / prod
 IS_PEGAZDEV=$2
 [[ $IS_PEGAZDEV == "false" ]] && sed -i "s|PROTO=.*|PROTO=\"https\"|g" "$PATH_PEGAZ_SERVICES/$1/config.sh"
+[[ $IS_PEGAZDEV == "false" ]] && sed -i "s|DRONE_RPC_=.*|DRONE_RPC_=\"https\"|g" "~/.drone-runner-exec/config" > /dev/null
 
 # drone config
 RPC_SECRET=$(openssl rand -hex 16)
 sed -i "s/RPC_SECRET=.*/RPC_SECRET=\"$RPC_SECRET\"/" "$PATH_PEGAZ_SERVICES/$1/config.sh"
+sed -i "s/DRONE_RPC_SECRET=.*/DRONE_RPC_SECRET=\"$RPC_SECRET\"/" "~/.drone-runner-exec/config" > /dev/null
