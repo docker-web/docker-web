059999fdf3b9d02455f1d3ff09e0c55fabed6b04
prosety <dev@prosety.com>
2022-10-13 14:31:49 +0200

    FIX: deluge dark theme

--- services/deluge/post-install.sh
+++ services/deluge/post-install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-DARK_PATH_HTML="/usr/lib/python3.10/site-packages/deluge/ui/web/index.html"
-DARK_INJECT='<link rel="stylesheet" type="text\/css" href="https:\/\/halianelf.github.io\/Deluge-Dark\/deluge.css">'
+DARK_PATH="/usr/lib/python3.10/site-packages/deluge/ui/web"
+DARK_PATH_HTML="${DARK_PATH}/index.html"
+DARK_INJECT='<link rel="stylesheet" type="text\/css" href="/css/deluge-dark.css">'
 DARK_SEARCH="<\/head>"
+
+docker cp $PATH_PEGAZ_SERVICES/$1/css/deluge.css deluge:$DARK_PATH/css
+docker cp $PATH_PEGAZ_SERVICES/$1/css/deluge-dark.css deluge:$DARK_PATH/css
+
+docker exec deluge sed -i "/deluge-dark.css/d" $DARK_PATH_HTML
 docker exec deluge sed -i "/$DARK_SEARCH/i \ \t$DARK_INJECT" $DARK_PATH_HTML
