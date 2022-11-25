d5a2cc704ad5fdc0b284282b76e90ea11a4b2d64
prosety <dev@prosety.com>
2022-11-25 19:42:48 +0100

    FIX: minors fixes

--- services/deluge/config.sh
+++ services/deluge/config.sh
@@ -2,5 +2,5 @@
 export DOMAIN="deluge.$MAIN_DOMAIN"
 export PORT="8112"
 export PORT_EXPOSED="8112"
-export REDIRECTIONS=""
+export REDIRECTIONS="torrent->/ torrents->/"
 export POST_INSTALL_TEST_CMD="docker exec deluge ls"
