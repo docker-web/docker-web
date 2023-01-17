fbbf9e29d6f7451293a8f454bb118e4ded394765
valere <dev@valerebron.com>
2023-01-17 20:28:37 +0100

    FIX: add deluge responsive viewport

--- services/deluge/post-install.sh
+++ services/deluge/post-install.sh
@@ -2,6 +2,7 @@
 DARK_PATH="/usr/lib/python3.11/site-packages/deluge/ui/web"
 DARK_PATH_HTML="${DARK_PATH}/index.html"
 DARK_INJECT='<link rel="stylesheet" type="text\/css" href="/css/deluge-dark.css">'
+DARK_INJECT_2='<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">'
 DARK_SEARCH="<\/head>"
 
 docker cp $PATH_PEGAZ_SERVICES/$1/css/deluge.css deluge:$DARK_PATH/css
@@ -10,3 +11,4 @@ docker cp $PATH_PEGAZ_SERVICES/$1/images deluge:$DARK_PATH/images
 
 docker exec deluge sed -i "/deluge-dark.css/d" $DARK_PATH_HTML
 docker exec deluge sed -i "/$DARK_SEARCH/i \ \t$DARK_INJECT" $DARK_PATH_HTML
+docker exec deluge sed -i "/$DARK_SEARCH/i \ \t$DARK_INJECT_2" $DARK_PATH_HTML
