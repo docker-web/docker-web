5df728ecd8d65baf91b700156b38ad7d6778475c
valere <dev@valerebron.com>
2023-02-27 17:48:49 +0100

    FEAT: radio anim v2

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PEGAZ_VERSION="1.5.6"
+export PEGAZ_VERSION="1.5.7"
 export GITHUB_PEGAZ="https://github.com/valerebron/pegaz"
 export PATH_PEGAZ="/opt/pegaz"
 export PATH_PEGAZ_SERVICES="$PATH_PEGAZ/services"
