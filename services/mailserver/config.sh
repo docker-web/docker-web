a3090bf22cf8a7c1718c43ca0ef64fb75f9b2963
valere <dev@valerebron.com>
2023-06-27 14:56:19 +0200

    FIX: port for roundcube

--- services/mailserver/config.sh
+++ services/mailserver/config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOMAIN="webmail.$MAIN_DOMAIN"
 export MAILSERVER_DOMAIN="mailserver.$MAIN_DOMAIN"
-export PORT="7781"
+export PORT="7788"
 export PORT_EXPOSED="80"
 export REDIRECTIONS=""
