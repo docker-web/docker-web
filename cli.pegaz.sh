3da2736c5f4e5095851aa45322d7d34a2aabfff0
valere <dev@valerebron.com>
2023-06-29 14:24:45 +0200

    FEAT: list ports fn

--- cli.pegaz.sh
+++ cli.pegaz.sh
@@ -462,6 +462,30 @@ PORT() {
   echo "the last port used is $(GET_LAST_PORT)"
 }
 
+PORTS() {
+  SERVICES_PORTS=()
+  for PATH_SERVICE in $PATH_PEGAZ_SERVICES/*; do
+    if [[ -f "$PATH_SERVICE/$FILENAME_CONFIG" || -f "$PATH_SERVICE/$FILENAME_ENV" ]]; then
+      if [[ -f "$PATH_SERVICE/$FILENAME_CONFIG" ]]; then
+        SED_PREFIX="export PORT" && FILENAME=$FILENAME_CONFIG
+      else
+        SED_PREFIX="PORT" && FILENAME=$FILENAME_ENV
+      fi
+      CURRENT_PORT=$(sed -n "s/^$SED_PREFIX\(.*\)/\1/p" < "$PATH_SERVICE/$FILENAME")
+      CURRENT_PORT=$(echo "$CURRENT_PORT" | tr ' ' '\n' | grep -v '_EXPOSED=' | grep -o -E '[0-9]+' | sort -nr | head -n1)
+    fi
+    if [[ $CURRENT_PORT ]]; then
+      SERVICES_PORTS+=("${CURRENT_PORT}: $(basename "$PATH_SERVICE")")
+    fi
+  done
+
+  IFS=$'\n' SERVICES_PORTS=($(sort <<<"${SERVICES_PORTS[*]}"))
+
+  for service in "${SERVICES_PORTS[@]}"; do
+    echo "$service"
+  done
+}
+
 # SERVICE COMMANDS
 
 INIT() {
--- env.sh
+++ env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export PEGAZ_VERSION="1.6.0"
+export PEGAZ_VERSION="1.6.1"
 export GITHUB_PEGAZ="https://github.com/valerebron/pegaz"
 export PATH_PEGAZ="/opt/pegaz"
 export PATH_PEGAZ_SERVICES="$PATH_PEGAZ/services"
 export PATH_PEGAZ_BACKUP="$PATH_PEGAZ/backup"
 export PATH_BASHRC="/root/.bashrc"
-export COMMANDS_CORE="config help port uninstall upgrade version ps create storj"
+export COMMANDS_CORE="config help port ports uninstall upgrade version ps create storj"
 export COMMANDS_SERVICE="drop up start update reset logs backup restore storjbackup storjrestore state init post_install pre_install add_to_hosts"
 export COMMANDS_COMPOSE="build bundle config create down events exec help images kill pause port ps pull push restart rm run start stop top unpause up version"
 export COMMANDS="$COMMANDS_CORE $COMMANDS_SERVICE $COMMANDS_COMPOSE"
