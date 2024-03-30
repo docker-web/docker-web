56bcbb55fba88c43c5142fb8a724094c78f43e5f
valere <dev@valerebron.com>
2024-03-30 10:32:21 +0100

    cmd pegazdev only on pegaz folder

--- start.pegaz.sh
+++ start.pegaz.sh
@@ -1,3 +1,3 @@
 alias pegaz='bash /opt/pegaz/cli.pegaz.sh $1 $2'
-alias pegazdev='rsync -avq --exclude=.* ./ /opt/pegaz && bash cli.pegaz.sh $1 $2 '
+alias pegazdev='[ "$(basename "$PWD")" = "pegaz" ] && rsync -avq --exclude=.* ./ /opt/pegaz && bash cli.pegaz.sh $1 $2  || echo "Current directory is not 'pegaz'."'
 [[ -f "/opt/pegaz/completion.sh" ]] && source /opt/pegaz/completion.sh
