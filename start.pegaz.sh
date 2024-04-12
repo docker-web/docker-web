cd0d473fe591dd7389d4b53491d7391b18725113
valere <dev@valerebron.com>
2024-04-12 12:09:56 +0200

    pegaz -> docker-web

--- start.pegaz.sh
+++
@@ -1,4 +0,0 @@
-docker run -d --restart unless-stopped --name docker-web -v /var/run/docker.sock:/tmp/docker.sock:ro docker-web/docker-web
-alias pegaz='docker exec docker-web bash docker-web.sh $@'
-alias pegazdev='[ -f "./docker-web.sh" ] && bash docker-web.sh $@  || echo "there is no docker-web.sh here"'
-[[ -f "/opt/pegaz/completion.sh" ]] && source /opt/pegaz/completion.sh
