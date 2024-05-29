46ed729fa4335fb5a665408e47170bb1c5a304d1
valere <dev@valerebron.com>
2024-05-29 19:04:44 +0200

    generate gitea token

--- apps/gitea/post-install.sh
+++ apps/gitea/post-install.sh
@@ -2,6 +2,10 @@
 GITEA() {
     docker exec gitea gitea $1
 }
+GENERATE_TOKEN() {
+  echo $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $1 | head -n 1 | tr -d '\n')
+}
+
 # SSH Container Passthrough
 sudo useradd git >/dev/null 2>&1
 sudo chown -R git:git /home/git/
@@ -16,7 +20,7 @@ sudo chmod +x /usr/local/bin/gitea
 docker exec -u root gitea chown -R git:git /data/git/.ssh
 
 # CONFIGURE
-cp $PATH_DOCKERWEB_APPS/gitea/gitea.ini $PATH_DOCKERWEB_APPS/gitea/app.ini
+cp $PATH_DOCKERWEB_APPS/gitea/config.ini $PATH_DOCKERWEB_APPS/gitea/app.ini
 sed -i "s/\${USERNAME}/$USERNAME/g" $PATH_DOCKERWEB_APPS/gitea/app.ini
 sed -i "s/\${EMAIL}/$EMAIL/g" $PATH_DOCKERWEB_APPS/gitea/app.ini
 sed -i "s/\${PASSWORD}/$PASSWORD/g" $PATH_DOCKERWEB_APPS/gitea/app.ini
@@ -26,6 +30,10 @@ sed -i "s/\${PORT_DB}/$PORT_DB/g" $PATH_DOCKERWEB_APPS/gitea/app.ini
 sed -i "s/\${DOMAIN}/$DOMAIN/g" $PATH_DOCKERWEB_APPS/gitea/app.ini
 sed -i "s/\${PORT_SSH}/$PORT_SSH/g" $PATH_DOCKERWEB_APPS/gitea/app.ini
 sed -i "s/\${PORT_SSH_EXPOSED}/$PORT_SSH_EXPOSED/g" $PATH_DOCKERWEB_APPS/gitea/app.ini
+
+sed -i "s/\${LFS_JWT_SECRET}/$(GENERATE_TOKEN 43)/g" $PATH_DOCKERWEB_APPS/gitea/app.ini
+sed -i "s/\${INTERNAL_TOKEN}/$(GENERATE_TOKEN 36).$(GENERATE_TOKEN 24).$(GENERATE_TOKEN 43)/g" $PATH_DOCKERWEB_APPS/gitea/app.ini
+sed -i "s/\${JWT_SECRET}/$(GENERATE_TOKEN 43)/g" $PATH_DOCKERWEB_APPS/gitea/app.ini
 docker cp $PATH_DOCKERWEB_APPS/gitea/app.ini gitea:/etc/gitea/app.ini
 rm $PATH_DOCKERWEB_APPS/gitea/app.ini
 bash "$PATH_DOCKERWEB/src/cli.sh" restart gitea
