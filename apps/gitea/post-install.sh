fa6cf7938056cba31c72a1c8d392cc969511b6f7
valere <dev@valerebron.com>
2024-06-01 15:53:23 +0200

    try new ssh passthrough

--- apps/gitea/post-install.sh
+++ apps/gitea/post-install.sh
@@ -6,19 +6,6 @@ GENERATE_TOKEN() {
   echo $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $1 | head -n 1 | tr -d '\n')
 }
 
-# SSH Container Passthrough
-sudo useradd git >/dev/null 2>&1
-sudo chown -R git:git /home/git/
-sudo -u git touch /home/git/.ssh/id_rsa
-sudo -u git chmod 600 /home/git/.ssh/id_rsa
-sudo -u git chown -R git:git /home/git/
-sudo -u git ssh-keygen -q -t rsa -b 4096 -C "Gitea Host Key" -N "" -f /home/git/.ssh/id_rsa <<<y > /dev/null
-sudo -u git cat /home/git/.ssh/id_rsa.pub | sudo -u git tee -a /home/git/.ssh/authorized_keys > /dev/null
-sudo echo -e "#!/bin/bash
-ssh -p ${PORT_SSH} -o StrictHostKeyChecking=no git@127.0.0.1 \"SSH_ORIGINAL_COMMAND=\\\"\$SSH_ORIGINAL_COMMAND\\\" \$0 \$@\"" | sudo tee /usr/local/bin/gitea > /dev/null
-sudo chmod +x /usr/local/bin/gitea
-docker exec -u root gitea chown -R git:git /data/git/.ssh
-
 # CONFIGURE
 cp $PATH_DOCKERWEB_APPS/gitea/gitea.ini $PATH_DOCKERWEB_APPS/gitea/app.ini
 sed -i "s/\${USERNAME}/$USERNAME/g" $PATH_DOCKERWEB_APPS/gitea/app.ini
--- src/env.sh
+++ src/env.sh
@@ -1,4 +1,4 @@
-export DOCKERWEB_VERSION="24.6.1"
+export DOCKERWEB_VERSION="24.6.2"
 export GITHUB_DOCKERWEB="https://github.com/docker-web/docker-web"
 export PATH_DOCKERWEB=~/docker-web
 export PATH_DOCKERWEB_APPS=$PATH_DOCKERWEB/apps
