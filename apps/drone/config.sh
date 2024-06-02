5a01ce304319186e014b66b7d3d2a57fb27b5766
valere <dev@valerebron.com>
2024-06-02 10:46:37 +0200

    fix drone

--- apps/drone/config.sh
+++ apps/drone/config.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 export PORT="7725"
 export DOMAIN="drone.$MAIN_DOMAIN"
-export DOMAIN_GITEA="gitea.domain.com"
+export DOMAIN_GITEA="git.domain.com"
 export RPC_SECRET=""
 export GITEA_CLIENT_ID="d81353e9-773b-4270-bcb0-494490c6d305"
 export GITEA_CLIENT_SECRET="cy5LE7IRS9MDPqjX07_kZBi0FrDnfkUZYgafYl2BAyU="
--- src/env.sh
+++ src/env.sh
@@ -1,4 +1,4 @@
-export DOCKERWEB_VERSION="24.6.4"
+export DOCKERWEB_VERSION="24.6.5"
 export GITHUB_DOCKERWEB="https://github.com/docker-web/docker-web"
 export PATH_DOCKERWEB=~/docker-web
 export PATH_DOCKERWEB_APPS=$PATH_DOCKERWEB/apps
