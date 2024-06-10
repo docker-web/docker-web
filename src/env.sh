38811f4cbb0c4ec2d4caf1ff4f69ee7e2444e146
valere <dev@valerebron.com>
2024-06-10 20:42:43 +0200

    no ~ path please

--- src/env.sh
+++ src/env.sh
@@ -1,4 +1,4 @@
-export DOCKERWEB_VERSION="24.6.15"
+export DOCKERWEB_VERSION="24.6.16"
 export GITHUB_DOCKERWEB="https://github.com/docker-web/docker-web"
 export PATH_DOCKERWEB=~/docker-web
 export PATH_DOCKERWEB_APPS=$PATH_DOCKERWEB/apps
