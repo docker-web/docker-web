e07385d0f74f6265e0cdac99759a7fef0855918c
valere <dev@valerebron.com>
2024-08-06 17:30:03 +0200

    FIX: backup/restore multi-apps compatible

--- src/apps/backup.sh
+++ src/apps/backup.sh
@@ -1,3 +1,36 @@
 BACKUP() {
-  BACKUP_OR_RESTORE $1 "backup"
+  local PATH_BACKUP_APP=$PATH_DOCKERWEB_BACKUP/$1
+  mkdir -p $PATH_BACKUP_APP
+
+  # test
+  # if app exist
+  APP_STATE=$(GET_STATE $1)
+  if [ -n "$APP_STATE" ]
+  then
+    echo "[*] backup $1"
+    cd $PATH_DOCKERWEB_APPS/$1 && tar czf $PATH_BACKUP_APP/app.tar.gz *
+
+    # 0. up and down
+    [[ -z $(GET_STATE $1) ]] && EXECUTE "up -d" $1
+    EXECUTE "pause" $1
+    # 1. volume
+    for VOLUME in $(EXECUTE "config --volumes" $1)
+    do
+      local VOLUME=($(docker volume inspect --format "{{.Name}} {{.Mountpoint}}" "$1_$VOLUME" 2> /dev/null))
+      local NAME_VOLUME=${VOLUME[0]}
+      if [[ -n $NAME_VOLUME ]]
+      then
+        docker run --rm -v $NAME_VOLUME:/$NAME_VOLUME -v $PATH_BACKUP_APP:/backup busybox tar czf /backup/$NAME_VOLUME.tar.gz /$NAME_VOLUME 2> /dev/null
+      fi
+    done
+    # 2. tar & upload
+    cd $PATH_BACKUP_APP && tar czf $PATH_DOCKERWEB_BACKUP/$1.tar.gz *
+    [[ ! -z $STORJ_BUCKET_NAME ]] && uplink cp --progress $PATH_DOCKERWEB_BACKUP/$1.tar.gz sj://$STORJ_BUCKET_NAME
+
+    # 3. drop backup if storj is configured
+    [[ ! -z $STORJ_BUCKET_NAME ]] && rm -rf $PATH_DOCKERWEB_BACKUP/$1.tar.gz*
+    EXECUTE "unpause" $1
+    rm -rf $PATH_BACKUP_APP
+    echo "[√] backup $1 done"
+  fi
 }
--- src/apps/restore.sh
+++ src/apps/restore.sh
@@ -1,3 +1,35 @@
 RESTORE() {
-  BACKUP_OR_RESTORE $1 "restore"
+  # test
+  # restore if backup exist
+  if [ -f "$PATH_DOCKERWEB_BACKUP/$1.tar.gz" ]
+  then
+    local PATH_BACKUP_APP=$PATH_DOCKERWEB_BACKUP/$1
+    mkdir -p $PATH_BACKUP_APP
+    echo "[*] restore $1"
+    # 0. download & untar
+    [[ ! -z $STORJ_BUCKET_NAME ]] && uplink cp --progress -r sj://$STORJ_BUCKET_NAME/$1.tar.gz $PATH_DOCKERWEB_BACKUP
+    tar xf $PATH_DOCKERWEB_BACKUP/$1.tar.gz -C $PATH_BACKUP_APP
+
+    # 1. app
+    [[ -f "$PATH_BACKUP_APP/app.tar.gz" ]] && rm -rf $PATH_BACKUP_APP/$1/* && tar xf $PATH_BACKUP_APP/app.tar.gz -C $PATH_DOCKERWEB_APPS/$1
+
+    # 2. up and down
+    [[ -z $(GET_STATE $1) ]] && EXECUTE "up -d" $1
+    EXECUTE "stop" $1
+    # 3. volume
+    for VOLUME in $(EXECUTE "config --volumes" $1)
+    do
+      local VOLUME=($(docker volume inspect --format "{{.Name}} {{.Mountpoint}}" "$1_$VOLUME" 2> /dev/null))
+      local NAME_VOLUME=${VOLUME[0]}
+      if [[ -n $NAME_VOLUME ]]
+      then
+        docker run --rm -v $NAME_VOLUME:/$NAME_VOLUME -v $PATH_BACKUP_APP:/backup busybox sh -c "cd /$NAME_VOLUME && tar xf /backup/$NAME_VOLUME.tar.gz --strip 1"
+      fi
+    done
+
+    # 4. drop backup if storj is configured
+    EXECUTE "start" $1
+    rm -rf $PATH_BACKUP_APP
+    echo "[√] $1 restore done"
+  fi
 }
