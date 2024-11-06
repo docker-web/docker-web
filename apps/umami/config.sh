3ce5b72c75b51aaa9ae80ef8c9e8b703ac5ebc00
valere <dev@valerebron.com>
2024-11-06 11:56:03 +0100

    FEAT: add umami

---
+++ apps/umami/config.sh
@@ -0,0 +1,5 @@
+export DOMAIN="umami.$MAIN_DOMAIN"
+export PORT="7831"
+export PORT_DB="7832"
+export PORT_EXPOSED="3000"
+export REDIRECTIONS="" # example.$MAIN_DOMAIN->/route $MAIN_DOMAIN->url /route->/another-route /route->url
