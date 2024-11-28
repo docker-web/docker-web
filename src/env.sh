4231d6e825eb3d71ddd67f994849afeb4a4f1bdf
valere <dev@valerebron.com>
2024-11-28 20:42:01 +0100

    ICON: umami

--- src/env.sh
+++ src/env.sh
@@ -1,4 +1,4 @@
-export DOCKERWEB_VERSION="24.11.3"
+export DOCKERWEB_VERSION="24.11.4"
 export GITHUB_DOCKERWEB="https://github.com/docker-web/docker-web"
 export PATH_DOCKERWEB=/var/docker-web
 export PATH_DOCKERWEB_APPS=$PATH_DOCKERWEB/apps
