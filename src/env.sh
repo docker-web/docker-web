a57c3450682b1e15fc5c2332b3a33c55d7a1a35f
valere <dev@valerebron.com>
2025-03-03 20:37:22 +0100

    feat(core): improve dev mode

--- src/env.sh
+++ src/env.sh
@@ -1,4 +1,4 @@
-export DOCKERWEB_VERSION="25.2.1"
+export DOCKERWEB_VERSION="25.3.1"
 export GITHUB_DOCKERWEB="https://github.com/docker-web/docker-web"
 export PATH_DOCKERWEB=/var/docker-web
 export PATH_DOCKERWEB_APPS=$PATH_DOCKERWEB/apps
--- src/helpers/dev_mode.sh
+++ src/helpers/dev_mode.sh
@@ -3,20 +3,6 @@ DEV_MODE() {
   export IS_DEVMODE
   if [ $IS_DEVMODE ]  # if we work/dev from a folder named 'docker-web'
   then
-    FOLDERS_TO_EXCLUDE=(.git node_modules)
-    FILES_TO_EXCLUDE=(config.sh)
-    for i in *
-    do
-      SKIP=""
-      for j in "${FOLDERS_TO_EXCLUDE[@]}"
-      do
-        [ "$i" == "$j" ] && SKIP=1 && break
-      done
-      for j in "${FILES_TO_EXCLUDE[@]}"
-      do
-        [ "$i" == "$j" ] && SKIP=1 && break
-      done
-      [ -z "$SKIP" ] && cp -R "$i" "$PATH_DOCKERWEB/" # we should copy files to app in /var/docker-web
-    done
+    rsync -avz --quiet --exclude=".*/" --exclude="node_modules/" --exclude="config.sh" "./" "$PATH_DOCKERWEB/"  # we should copy files to app in /var/docker-web
   fi
 }
