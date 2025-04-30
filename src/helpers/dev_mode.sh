a501794c0c0de4445b8f32291986bd00d0c8cdb6
valere <dev@valerebron.com>
2025-04-30 04:41:04 +0200

    fix: devmode condition

--- src/helpers/dev_mode.sh
+++ src/helpers/dev_mode.sh
@@ -1,7 +1,7 @@
 DEV_MODE() {
   [ "$(basename "$PWD")" = "docker-web" ] && IS_DEVMODE=true || IS_DEVMODE=false
   export IS_DEVMODE
-  if [ $IS_DEVMODE ]  # if we work/dev from a folder named 'docker-web'
+  if [ "$IS_DEVMODE" = true ]  # if we work/dev from a folder named 'docker-web'
   then
     rsync -avz --quiet --exclude=".*/" --exclude="node_modules/" --exclude="config.sh" "./" "$PATH_DOCKERWEB/"  # we should copy files to app in /var/docker-web
   fi
