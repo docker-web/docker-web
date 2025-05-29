eb3575c5bc83232a9c725cbd668e95c1b834c7e9
valere <dev@valerebron.com>
2025-05-29 19:43:41 +0200

    re-add joal

---
+++ apps/joal/config.sh
@@ -0,0 +1,4 @@
+export DOMAIN="joal.$MAIN_DOMAIN"
+export PORT="7827"
+export PORT_EXPOSED="80"
+export REDIRECTIONS="" # example.$MAIN_DOMAIN->/route $MAIN_DOMAIN->url /route->/another-route /route->url
