640240559c3f4c6081a2933a0caaada456c27efd
valere <dev@valerebron.com>
2025-08-14 21:58:18 +0200

    fix: hide scrollbar tv player

--- src/env.sh
+++ src/env.sh
@@ -1,4 +1,4 @@
-export DOCKERWEB_VERSION="25.8.4"
+export DOCKERWEB_VERSION="25.8.5"
 export GITHUB_DOCKERWEB="https://github.com/docker-web/docker-web"
 export PATH_DOCKERWEB=/var/docker-web
 export PATH_DOCKERWEB_APPS=$PATH_DOCKERWEB/apps
