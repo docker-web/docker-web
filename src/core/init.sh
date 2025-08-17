68fbf8e96c8b763760331bf80b74be51e093e38b
valere <dev@valerebron.com>
2025-08-17 18:23:18 +0200

    FIX: drop unused cp in INIT cmd

--- src/core/init.sh
+++ src/core/init.sh
@@ -14,19 +14,13 @@ INIT() {
     mkdir -p $FOLDER
   fi
   local NAME=$(basename $FOLDER)
-echo $FOLDER
-  cp $PATH_DOCKERWEB/template/* $FOLDER/
-  cp $PATH_DOCKERWEB/template/.* $FOLDER/ > /dev/null 2>&1
+
+  cp -R $PATH_DOCKERWEB/template/* $FOLDER/
+  cp -R $PATH_DOCKERWEB/template/.github $FOLDER/ > /dev/null 2>&1
 
   sed -i "s|__PORT__|$(GET_LAST_PORT)|g" $FOLDER/config.sh
   sed -i "s|__APP_NAME__|$NAME|g" $FOLDER/docker-compose.yml
   sed -i "s|__APP_NAME__|$NAME|g" $FOLDER/README.md
   sed -i "s|__APP_NAME__|$NAME|g" $FOLDER/config.sh
   sed -i "s|DOMAIN=.*|DOMAIN=\"$NAME.\$MAIN_DOMAIN\"|g" $FOLDER/config.sh
-
-  if [ $IS_DEVMODE ]
-  then
-    mkdir -p $PATH_DOCKERWEB_APPS/$NAME
-    cp $FOLDER/* $PATH_DOCKERWEB_APPS/$NAME
-  fi
 }
