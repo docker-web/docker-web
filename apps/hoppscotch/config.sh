2583edd1406720d3b5957a486bb8156380997a24
valere <dev@valerebron.com>
2025-08-20 13:34:04 +0200

    drop old apps & add excalidraw back

--- apps/hoppscotch/config.sh
+++
@@ -1,4 +0,0 @@
-export DOMAIN="hoppscotch.$MAIN_DOMAIN"
-export PORT="7775"
-export PORT_EXPOSED="3000"
-export REDIRECTIONS=""
--- apps/plausible/config.sh
+++
@@ -1,6 +0,0 @@
-export DOMAIN="plausible.$MAIN_DOMAIN"
-export PORT="7717"
-export PORT_DB="7718"
-export PORT_EXPOSED="8000"
-export SECRET_KEY_BASE="VR65h9ZodCBVRxTExsDe55ZtWBc2Qn04c5jGRl0qYNWYNmpmKIKvZOaapNvbRYp3 eHJ0QLtQnN10audJTfYLZg=="
-export REDIRECTIONS=""
