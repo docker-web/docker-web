273674d2e835b54d4024fd32cd441b1a10ea025f
valere <dev@valerebron.com>
2025-08-28 16:54:19 +0200

    fix upgrade

--- src/core/upgrade.sh
+++ src/core/upgrade.sh
@@ -6,12 +6,8 @@ UPGRADE() {
   rm -rf /tmp/docker-web
   git clone --depth 1 $URL_DOCKERWEB_GITHUB /tmp/docker-web
 
-  rm -rf $PATH_DOCKERWEB/src $PATH_DOCKERWEB/docs
+  rm -rf $PATH_DOCKERWEB/src
   mv /tmp/docker-web/src $PATH_DOCKERWEB
-  mv /tmp/docker-web/docs $PATH_DOCKERWEB
-
-  rsync -raz --ignore-existing /tmp/docker-web/apps/* $PATH_DOCKERWEB_APPS
-  rsync -raz --exclude "$PATH_DOCKERWEB_APPS/launcher/web/index.html" --exclude "*config.sh" /tmp/docker-web/apps/* $PATH_DOCKERWEB_APPS
 
   source $PATH_DOCKERWEB/src/env.sh
   echo "[√] docker-web is now upgraded (v$OLD_DOCKERWEB_VERSION -> v$DOCKERWEB_VERSION)"
