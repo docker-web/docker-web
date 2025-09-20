fbc35deeb859f5010395d1ff1505ec3510f67b29
valere <dev@valerebron.com>
2025-09-20 10:44:34 +0200

    fix image & container cache

--- src/apps/up.sh
+++ src/apps/up.sh
@@ -2,8 +2,8 @@ UP() {
   ADD_TO_HOSTS $1
   SETUP_PROXY
   PRE_INSTALL $1
-  EXECUTE "build" $1
-  EXECUTE "up -d" $1
+  EXECUTE "build --no-cache $1"
+  EXECUTE "up -d --force-recreate" $1
   POST_INSTALL $1
   UPDATE_LAUNCHER $1
   APP_INFOS $1
