935a88b6a16f3e5e19de619f2dc986c587e94eda
valere <dev@valerebron.com>
2025-12-02 21:58:14 +0100

    FEAT: upgrade cmd for nuxt template

--- src/env.sh
+++ src/env.sh
@@ -1,4 +1,4 @@
-export DOCKERWEB_VERSION="25.12.1"
+export DOCKERWEB_VERSION="25.12.2"
 export PATH_DOCKERWEB=/var/docker-web
 export PATH_APPS=$PATH_DOCKERWEB/apps
 export PATH_BACKUP=$PATH_DOCKERWEB/backup
