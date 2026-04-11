7b7f8b375ec8331459e9652f8867add34880906a
Valère <contact@valere.dev>
2026-04-11 10:43:17 +0200

    config.sh -> env.sh

--- apps/penpot/pre-install.sh
+++ apps/penpot/pre-install.sh
@@ -3,5 +3,5 @@
 # si /var/docker-web/config.sh à PENPOT_SECRET vide, alors :
 if [ -z "$PENPOT_SECRET" ]; then
     key=$(python3 -c "import secrets; print(secrets.token_urlsafe(64))")
-    echo "export PENPOT_SECRET=\"$key\"" >> $PATH_DOCKERWEB/config.sh
+    echo "export PENPOT_SECRET=\"$key\"" >> $PATH_DOCKERWEB/env.sh
 fi
